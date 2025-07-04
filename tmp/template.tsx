d3f712c05bcef5aab091a23d641dd5ba37d95371
Sergii Rudchyk <sergii@drkfrst.com>
2025-07-04 15:17:28 +0300

    fix tmp

--- tmp/template.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { DefaultLayout } from '@/lib/layouts';
-
-export default function Template({ children }: { children: React.ReactNode }) {
-  return <DefaultLayout>{children}</DefaultLayout>;
-}
