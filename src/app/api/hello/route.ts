86e695ef5d378e0cf3a77d8da7c8a04eca0851c8
Sergii Rudchyk <sergii@drkfrst.com>
2025-08-07 14:43:39 +0300

    d

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,8 +1,14 @@
-// import { logger } from '@/lib/services/logger.service';
-import { NextResponse } from 'next/server';
+import { logger } from '@/lib/services/logger.service';
+import { NextRequest, NextResponse } from 'next/server';
 
-// export async function GET(req: Request) {
-export async function GET() {
-  // logger.info({ url: req.url, method: req.method }, 'Incoming request');
+export async function GET(req: NextRequest) {
+  console.log('req.url', req.url);
+  console.log('req.method', req.method);
+
+  try {
+    logger.info({ url: req.url, method: req.method }, 'Incoming request');
+  } catch (error) {
+    console.log('error', error);
+  }
   return NextResponse.json({ message: 'Hello, from API!' });
 }
