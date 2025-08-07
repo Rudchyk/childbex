bcf1a8f8133c8b1d2f7e6b34909d31ffa73fcbbd
Sergii Rudchyk <sergii@drkfrst.com>
2025-08-07 12:55:55 +0300

    s

--- server.ts
+++ server.ts
@@ -3,10 +3,10 @@ import path from 'path';
 import { createServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
-// import helmet from 'helmet';
-// import compression from 'compression';
+import helmet from 'helmet';
+import compression from 'compression';
 import morgan from 'morgan';
-// import cors from 'cors';
+import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 
 const port = parseInt(process.env.PORT || '3000', 10);
@@ -20,64 +20,38 @@ const limiter = rateLimit({
 });
 
 // app.prepare().then(() => {
-//   server.use(
-//     helmet({
-//       contentSecurityPolicy: {
-//         directives: {
-//           defaultSrc: ["'self'", 'data:', 'blob:'],
-//           scriptSrc: [
-//             "'self'",
-//             "'unsafe-inline'",
-//             "'unsafe-eval'",
-//             'data:',
-//             'blob:',
-//           ],
-//           styleSrc: ["'self'", "'unsafe-inline'", 'data:'],
-//           imgSrc: ["'self'", 'data:', 'blob:'],
-//           connectSrc: ["'self'", 'ws:', 'data:', 'blob:'],
-//           fontSrc: ["'self'", 'data:'],
-//           objectSrc: ["'none'"],
-//         },
-//       },
-//       crossOriginEmbedderPolicy: false,
-//       crossOriginResourcePolicy: { policy: 'cross-origin' },
-//     })
-//   );
-//   server.use(compression());
-//   server.use(morgan(process.env.NODE_ENV === 'production' ? 'tiny' : 'dev'));
-//
 //   server.use(cors());
 //   server.use(express.json({ limit: '10mb' }));
 //   server.use(express.urlencoded({ extended: true, limit: '10mb' }));
-
-//   server.use(
-//     '/uploads',
-//     express.static(path.join(__dirname, dev ? '' : '..', 'uploads'))
-//   );
-//   server.get('/health', (req, res) => {
-//     res.status(200).json({
-//       status: 'OK',
-//       timestamp: new Date().toISOString(),
-//       uptime: process.uptime(),
-//     });
-//   });
-//   server.use((req, res) => {
-//     const parsedUrl = parse(req.url!, true);
-//     return handle(req, res, parsedUrl);
-//   });
-//   const ser = createServer(server);
-
-//   ser.listen(port);
-
-//   console.log(
-//     `> Server listening at http://localhost:${port} as ${
-//       dev ? 'development' : process.env.NODE_ENV
-//     }`
-//   );
 // });
 
 app.prepare().then(() => {
   const server = express();
+  server.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'", 'data:', 'blob:'],
+          scriptSrc: [
+            "'self'",
+            "'unsafe-inline'",
+            "'unsafe-eval'",
+            'data:',
+            'blob:',
+          ],
+          styleSrc: ["'self'", "'unsafe-inline'", 'data:'],
+          imgSrc: ["'self'", 'data:', 'blob:'],
+          connectSrc: ["'self'", 'ws:', 'data:', 'blob:'],
+          fontSrc: ["'self'", 'data:'],
+          objectSrc: ["'none'"],
+        },
+      },
+      crossOriginEmbedderPolicy: false,
+      crossOriginResourcePolicy: { policy: 'cross-origin' },
+    })
+  );
+  server.use(cors());
+  server.use(compression());
   server.use(morgan(isDev ? 'dev' : 'tiny'));
   server.use(limiter);
   server.use(
