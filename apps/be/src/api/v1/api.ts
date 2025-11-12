cfbaea3943dcacfafc3d0fb92b6196e2b7f892d3
Sergii Rudchyk <sergii@drkfrst.com>
2025-11-12 07:08:10 +0200

    d

--- apps/be/src/api/v1/api.ts
+++ apps/be/src/api/v1/api.ts
@@ -10,41 +10,41 @@ import './routes/hello';
 import './routes/patients.api.routes';
 
 export const setupAPIRoutes = (app: Express, keycloak: KeycloakType) => {
-  Object.entries(router.openAPIDocument.paths || {}).forEach(
-    ([path, methods]) => {
-      if (path && methods) {
-        Object.entries(methods).forEach(([key, props]) => {
-          if (!Array.isArray(props) && 'security' in props && props.security) {
-            const route = apiRoute + path;
-            const method = key.toLowerCase() as
-              | 'get'
-              | 'post'
-              | 'put'
-              | 'delete'
-              | 'patch';
+  // Object.entries(router.openAPIDocument.paths || {}).forEach(
+  //   ([path, methods]) => {
+  //     if (path && methods) {
+  //       Object.entries(methods).forEach(([key, props]) => {
+  //         if (!Array.isArray(props) && 'security' in props && props.security) {
+  //           const route = apiRoute + path;
+  //           const method = key.toLowerCase() as
+  //             | 'get'
+  //             | 'post'
+  //             | 'put'
+  //             | 'delete'
+  //             | 'patch';
 
-            props.security?.forEach((securityItem) => {
-              if (Array.isArray(securityItem[SecuritiesKeysEnum.KEYCLOAK])) {
-                app[method](
-                  route,
-                  keycloak.protect(...securityItem[SecuritiesKeysEnum.KEYCLOAK])
-                );
-              } else if (
-                Array.isArray(securityItem[SecuritiesKeysEnum.KEYCLOAK_BEARER])
-              ) {
-                app[method](
-                  route,
-                  keycloak.protect(
-                    ...securityItem[SecuritiesKeysEnum.KEYCLOAK_BEARER]
-                  )
-                );
-              }
-            });
-          }
-        });
-      }
-    }
-  );
+  //           props.security?.forEach((securityItem) => {
+  //             if (Array.isArray(securityItem[SecuritiesKeysEnum.KEYCLOAK])) {
+  //               app[method](
+  //                 route,
+  //                 keycloak.protect(...securityItem[SecuritiesKeysEnum.KEYCLOAK])
+  //               );
+  //             } else if (
+  //               Array.isArray(securityItem[SecuritiesKeysEnum.KEYCLOAK_BEARER])
+  //             ) {
+  //               app[method](
+  //                 route,
+  //                 keycloak.protect(
+  //                   ...securityItem[SecuritiesKeysEnum.KEYCLOAK_BEARER]
+  //                 )
+  //               );
+  //             }
+  //           });
+  //         }
+  //       });
+  //     }
+  //   }
+  // );
 
   app.use(apiRoute + apiDocRoute, basicAuthValidator);
   app.use(router);
